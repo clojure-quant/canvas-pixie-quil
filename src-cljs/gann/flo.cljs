53a09310c0999a30dba519c7794ca22a8d028d9a
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-04-30 01:16:05 +0200

    alex demo complete

--- src-cljs/gann/flo.cljs
+++ src-cljs/gann/flo.cljs
@@ -3,6 +3,8 @@
       [pointslope.remit.events :as pse :refer [emit subscribe]]
       [pointslope.remit.middleware :as psm :refer [event-map-middleware]]
       [reagent.core :as r]
+      [gann.components :as c]
+      [gann.geometry :as g]
  ))
 
 ; http://jonibologna.com/svg-viewbox-and-viewport/
@@ -10,7 +12,31 @@
 ; https://github.com/awb99/reagent-svg-demo/blob/master/src/cljs/svg/app.cljs
 ; https://github.com/awb99/tictactoe/blob/master/src/tictactoe/core.cljs
 ; https://github.com/thi-ng/geom/
-;https://github.com/oakes/play-cljs-examples/blob/master/super-koalio/src/super_koalio/core.cljs
+; https://github.com/oakes/play-cljs-examples/blob/master/super-koalio/src/super_koalio/core.cljs
+; https://reagent-project.github.io/news/any-arguments.html
+
+(def log (.-log js/console))
+
+(defonce points
+  (r/atom
+   {:p1 (g/point 200 100)
+    :p2 (g/point 200 200)
+    :p3 (g/point 100 200)
+    :c (g/point 250 250)
+    :p (g/point 250 300)}))
+
+(defn get-bcr [svg-root]
+  (-> svg-root
+      r/dom-node
+      .getBoundingClientRect))
+
+(defn move-point [svg-root p]
+  (fn [x y]
+    (let [bcr (get-bcr svg-root)
+          xxx (log bcr)  ]
+      (swap! points assoc p (g/point (- x (.-left bcr)) (- y (.-top bcr)))))))
+
+
 
 (def size 500)
 
@@ -45,7 +71,7 @@
 (defn select-component
   [fill]
   (fn [fill]
-    (let [colors ["gray" "red" "orange" "yellow" "green" "blue"
+    (let [colors [ "rgb(100,10,70)" "gray" "red" "orange" "yellow" "green" "blue"
                   "aqua" "indigo" "purple" "brown" "black"]]
       (do
         [:div {:class "form-group"}
@@ -59,8 +85,7 @@
 
 
 
-(defn range-component
-  [v min max title]
+(defn range-component [v min max title]
   [:div {:class "form-group"}
    [:label {:for title} title [:small " [ " @v " ] "]]
    [:input {:type "range"
@@ -77,21 +102,23 @@
 (defonce app-db (atom {:x 360 :y 200 :radius 50 :fill "black" :clicks 0}))
 
 (defn fib-line [y x width color]
-      [:line { :y1 y :y2 y
-               :x1 x  :x2 (+ x width)  
+      [:line { :y1 y 
+               :y2 y
+               :x1 x  
+               :x2 (+ x width)  
                :style {:stroke color :stroke-width 2}}])
 
 
 (defn lines []
   [:g
-    [:line {:x1 100 :y1 100 :x2 500 :y2 100 :style {:stroke "rgb(255,0,0)" :stroke-width 2}}]
-    [:line {:x1 100 :y1 200 :x2 500 :y2 200 :style {:stroke "rgb(255,0,0)" :stroke-width 2}}]])
+    [:line {:x1 100 :y1 100 :x2 300 :y2 100 :style {:stroke "blue" :stroke-width 5}}]
+    [:line {:x1 100 :y1 200 :x2 350 :y2 200 :style {:stroke "green" :stroke-width 10}}]])
 
 
 (defn fib-sr [x width color]
   "fibonacci support-resistence"
-  (let [fibs [3.6 2.6 1.6 1.0 0.62 0.36]]
-    (fn [] 
+  (let [fibs [3.618 2.618 1.618 1.0 0.618 0.5 0.362]]
+    (fn [x width color] 
       [:g
         (for [y fibs] [fib-line (* 100 y) x width color ])
       ])))
@@ -102,12 +129,56 @@
  ;     [:line {:x1 100 :y1 200 :x2 500 :y2 200 :style {:stroke "rgb(255,0,0)" :stroke-width 2}}]
  ; ]
  
+ (def i 15)
+ (def f 5.555)
+ (def s "hallo alex")
+ ;(def freunde ["alex" "florian" "biene maya" "captain kirk"])
+ (def freunde [
+    {:alter 33 :land "deutschland" :name "alex" :vater {:name "kemal"} :kinder [ "a" "b" "c"]}
+    {:alter 43 :land "panama" :name "florian"}
+    {:alter 100 :land "mayastan" :name "biene maya"}
+    {:alter 150 :land "usa" :name "lucky luke"}
+ ] )
  
- 
+(defn drawing [svg-root fill]
+  (let [{:keys [p1 p2 p3 p c]} @points]
+  [:g
+    [:rect {:x 10 :y 10 :width 10 :height 10 :style {:fill @fill}}]
+        [:rect {:x 100 :y 50 :width 30 :height 30}]
+        [:rect {:x 200 :y 50 :width 30 :height 30 :style {:fill @fill}}]
+        [:rect {:x 300 :y 10 :width 30 :height 30 :style {:fill "blue"}}]
+        [:rect {:x 150 :y 100 :width 50 :height 50 :style {:fill "green"}}]
+        [:circle
+          {:r 10
+            :stroke "cyan"
+            :stroke-width 400
+            :fill "none"
+            :cx 100
+            :cy 150
+            :style {:stroke-width 200 }
+            }]
+        [fib-sr 50 50 @fill]
+        [fib-sr 200 50 "rgb(255,0,0)"]  ;red
+        [fib-sr 300 50 "blue"]
+        [fib-sr 10 30 @fill]
+       ;[lines]
+        [c/point {:on-drag (move-point svg-root :p1)} p1]
+  ]
+))
+
+
+(defn ist-sau-alt [person]
+  (let [sau-alt 40]
+     (if (>= (:alter person) sau-alt) true false)
+))
+
+(defn alters-farbe [person]
+  (if (ist-sau-alt person) :red :blue)
+)
 
 
 (defn demo []
- (let [size 600
+ (let [size 700
        view-size (r/atom 400)
        fill (r/atom "green")
       ]
@@ -118,27 +189,21 @@
              :view-box (str "0 0 " @view-size " " @view-size)
              :style {:background-color "gray"}
              }
-        [:rect {:x 10 :y 10 :width 10 :height 10 :style {:fill @fill}}]
-        [:rect {:x 100 :y 50 :width 30 :height 30}]
-        [:rect {:x 200 :y 50 :width 30 :height 30 :style {:fill @fill}}]
-        [:rect {:x 300 :y 10 :width 30 :height 30 :style {:fill "blue"}}]
-        [:circle
-          {:r 0.35
-            :stroke "green"
-            :stroke-width 400
-            :fill "none"
-            :cx 100
-            :cy 150
-            }]
-        [fib-sr 50 50 @fill]
-        [fib-sr 200 50 "rgb(255,0,0)"]  ;red
-        [fib-sr 300 50 "blue"]
-        [fib-sr 10 30 @fill]
+          [drawing (r/current-component) fill]
       ]
       [range-component view-size 0 size "view-size"]
       [select-component fill]
+      
+      [:h1 (str "View-Size: " @view-size)]
+      [:p (str "Addition:" (* 8 (- 100 30 )))]
       [:p (str "Color: " @fill)]
-      [:p (str "View-Size: " @view-size)]
+      [:ol (for [freund freunde] 
+             [:div 
+                [:li {:style {:color (alters-farbe freund)  }} 
+                     [:strong (:name freund)] (:alter freund) ]
+             ]
+                  
+              ) ]
       ]
       )))
      
